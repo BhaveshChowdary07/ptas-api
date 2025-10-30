0448774017139915e535d7327410a2fe20417ada
BhaveshChowdary07 <bhaveshsri777@gmail.com>
2025-10-30 15:18:07 +0530

    fix in login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { register, login, refresh } from '.authController.js';
+import { register, login, refresh } from "../controllers/authController.js";
 
 const router = express.Router();
 
